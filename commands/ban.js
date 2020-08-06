7cc51669842239288a747a2b23375bc91fb13dfb
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-06 20:02:00 -0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -6,34 +6,34 @@ module.exports = {
 
     async run (client, message, args) {
 
-        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!')
-        if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the right permissions.')
+        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('Você não pode usar isso.!')
+        if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('Eu não tenho a permissão certa.')
 
         const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
-        if(!args[0]) return message.channel.send('Please specify a user');
+        if(!args[0]) return message.channel.send('Por favor, especifique um usuário');
 
-        if(!member) return message.channel.send('Can\'t seem to find this user. Sorry \'bout that :/');
-        if(!member.bannable) return message.channel.send('This user can\'t be banned. It is either because they are a mod/admin, or their highest role is higher than mine');
+        if(!member) return message.channel.send('Não consigo encontrar esse usuário. Desculpe :/');
+        if(!member.bannable) return message.channel.send('Este usuário não pode ser banido. É porque eles são um moderador/administrador, ou seu papel mais alto é maior do que o meu');
 
-        if(member.id === message.author.id) return message.channel.send('Bruh, you can\'t ban yourself!');
+        if(member.id === message.author.id) return message.channel.send('Bruh, você não pode se banir.!');
 
         let reason = args.slice(1).join(" ");
 
-        if(reason === undefined) reason = 'Unspecified';
+        if(reason === undefined) reason = 'Não fornecido';
 
         member.ban(reason)
         .catch(err => {
-            if(err) return message.channel.send('Something went wrong')
+            if(err) return message.channel.send('Algo deu errado.')
         })
 
         const banembed = new Discord.MessageEmbed()
-        .setTitle('Member Banned')
+        .setTitle('Usuário Banido!')
         .setThumbnail(member.user.displayAvatarURL())
-        .addField('User Banned', member)
-        .addField('Kicked by', message.author)
-        .addField('Reason', reason)
-        .setFooter('Time kicked', client.user.displayAvatarURL())
+        .addField('Nome do usuário:', member)
+        .addField('Autor da punição:', message.author)
+        .addField('Motivação:', reason)
+        .setFooter(`Requisitado por ${message.author.tag}`, message.author.displayAvatarURL())
         .setTimestamp()
 
         message.channel.send(banembed);
