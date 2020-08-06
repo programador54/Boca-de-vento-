b2f811ab271c8d6d5667fabd7036898e2ef7e9cc
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-06 18:32:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const Discord = require("discord.js")
 const client = new Discord.Client() 
 const config = require('./config.json')
 
+client.on('message', message => {
+     if (message.author.bot) return;
+     if (message.channel.type == 'dm') return;
+     if (!message.content.toLowerCase().startsWith(config.prefix)) return;
+     if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) return;
+
+    const args = message.content
+        .trim().slice(config.prefix.length)
+        .split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    try {
+        const commandFile = require(`./commands/${command}.js`)
+        commandFile.run(client, message, args);
+    } catch (err) {
+    console.error('Erro:' + err);
+  }
+});
 
 client.on("ready", () => {
   let activities = [
