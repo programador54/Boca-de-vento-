11029963b369219021a5bfa364a4b1495dbf250c
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-06 18:20:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const Discord = require("discord.js") 
 const client = new Discord.Client() 
 const config = require('./config.json')
+
+
+client.on("ready", () => {
+  let activities = [
+      `Em desenvolvimento!`,
+      `Criador Linux ON!`,
+      `Segurança a todos!`,
+      `Paz mundial!`
+    ],
+    i = 0;
+  setInterval( () => client.user.setActivity(`${activities[i++ % activities.length]}`, {
+        type: "WATCHING"
+      }), 1000 * 60); 
+  client.user
+      .setStatus("dnd")
+      .catch(console.error);
+console.log("Estou Online!")
+});
+
 client.login(config.token)
            
