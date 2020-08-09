97a5d4ea1e795a5760c6e3bca55a9564189c73a6
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-09 16:57:50 -0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -6,17 +6,17 @@ module.exports = {
 
     async run (client, message, args) {
 
-        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('<a:alertA:727101012174962838> | <@${message.author.id}>. Você precisa ter a permissão de **BANIR_MEMBROS** para poder utilizar este comando.')
-        if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('Eu não tenho a permissão certa.')
+        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`<a:alertA:727101012174962838> | <@${message.author.id}>, Você precisa ter a permissão de **BANIR_MEMBROS** para poder utilizar este comando.`).then(msg => msg.delete({timeout: 5000}))
+        if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send(`<a:errado:630163775122833419> | <@${message.author.id}>, Eu não tenho a permissão certa.').then(msg => msg.delete({timeout: 5000}))
 
         const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
-        if(!args[0]) return message.channel.send('Por favor, especifique um usuário');
+        if(!args[0]) return message.channel.send(`<a:errado:630163775122833419> | <@${message.author.id}>, Por favor, especifique um usuário\n\> **ex**: d-ban @user flood no chat').then(msg => msg.delete({timeout: 5000}));
 
         if(!member) return message.channel.send('Não consigo encontrar esse usuário. Desculpe :/');
-        if(!member.bannable) return message.channel.send('Este usuário não pode ser banido. É porque eles são um moderador/administrador, ou seu papel mais alto é maior do que o meu');
+        if(!member.bannable) return message.channel.send(`<a:errado:630163775122833419> | <@${message.author.id}>, Este usuário não pode ser banido. É porque eles são um moderador/administrador, ou seu papel mais alto é maior do que o meu`).then(msg => msg.delete({timeout: 5000}));
 
-        if(member.id === message.author.id) return message.channel.send('Bruh, você não pode se banir.!');
+        if(member.id === message.author.id) return message.channel.send(`<a:errado:630163775122833419> | <@${message.author.id}>, Ops , você não pode se banir.`).then(msg => msg.delete({timeout: 5000}));
 
         let reason = args.slice(1).join(" ");
 
@@ -24,7 +24,7 @@ module.exports = {
 
         member.ban(reason)
         .catch(err => {
-            if(err) return message.channel.send('Algo deu errado.')
+            if(err) return message.channel.send('<a:errado:630163775122833419> | <@${message.author.id}>, Algo deu errado.').then(msg => msg.delete({timeout: 5000}));
         })
 
         const banembed = new Discord.MessageEmbed()
