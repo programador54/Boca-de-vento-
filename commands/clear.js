91fb46fc18cca9f59f6dd28b531473fd8c010e4b
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-09 17:01:27 -0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -2,14 +2,10 @@ const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
   if (!message.member.permissions.has("MANAGE_MESSAGES"))
-    return message.reply(
-      "você é fraco, lhe falta permissão de `Gerenciar Mensagens` para usar esse comando"
-    );
+    return message.channel.send(`<a:alertA:727101012174962838> | <@${message.author.id}>. Você precisa ter a permissão de **GERENCIAR_MENSSAGENS** para poder utilizar este comando.`).then(msg => msg.delete({timeout: 5000}));
   const deleteCount = parseInt(args[0], 10);
   if (!deleteCount || deleteCount < 1 || deleteCount > 99)
-    return message.reply(
-      "forneça um número de até **99 mensagens** a serem excluídas"
-    );
+    return message.channel.send(`<a:errado:630163775122833419> | <@${message.author.id}>, Forneça um número junto ao comando de até **99 mensagens** a serem excluídas!`).then(msg => msg.delete({timeout: 5000}));
 
   const fetched = await message.channel.messages.fetch({
     limit: deleteCount + 1
