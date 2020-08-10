00c971147101eeff3e5eda79b6cde3eff9a9a327
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-10 11:00:30 -0300

    Create giveaway.js

---
+++ commands/giveaway.js
@@ -0,0 +1,53 @@
+const { MessageEmbed } = require("discord.js");
+const ms = require("ms");
+module.exports = {
+  name: "giveaway",
+  description: "Create a simple giveaway",
+  usage: "<time> <channel> <prize>",
+  category: "fun",
+  run: async (bot, message, args) => {
+    if (!args[0]) return message.channel.send(`Você não especificou seu tempo!`);
+    if (
+      !args[0].endsWith("d") &&
+      !args[0].endsWith("h") &&
+      !args[0].endsWith("m")
+    )
+      return message.channel.send(
+        `Você não usou a formatação correta para o tempo!`
+      );
+    if (isNaN(args[0][0])) return message.channel.send(`That is not a number!`);
+    let channel = message.mentions.channels.first();
+    if (!channel)
+      return message.channel.send(
+        `Eu não consegui encontrar esse canal no servidor!`
+      );
+    let prize = args.slice(2).join(" ");
+    if (!prize) return message.channel.send(`Nenhum prêmio especificado!`);
+    message.channel.send(`*Sorteio criado em ${channel}*`);
+    let Embed = new MessageEmbed()
+      .setTitle(`New giveaway!`)
+      .setDescription(
+        `The user ${message.author} is hosting a giveaway for the prize of **${prize}**`
+      )
+      .setTimestamp(Date.now() + ms(args[0]))
+      .setColor(`BLUE`);
+    let m = await channel.send(Embed);
+    m.react("🎉");
+    setTimeout(() => {
+      if (m.reactions.cache.get("🎉").count <= 1) {
+        message.channel.send(`Reactions: ${m.reactions.cache.get("🎉").count}`);
+        return message.channel.send(
+          `Not enough people reacted for me to start draw a winner!`
+        );
+      }
+
+      let winner = m.reactions.cache
+        .get("🎉")
+        .users.cache.filter((u) => !u.bot)
+        .random();
+      channel.send(
+        `The winner of the giveaway for **${prize}** is... ${winner}`
+      );
+    }, ms(args[0]));
+  },
+};
