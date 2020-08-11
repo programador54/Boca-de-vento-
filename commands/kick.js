0341cff07bad667fa9161d060248171f0f69fbda
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-11 12:46:08 -0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -9,12 +9,12 @@ module.exports = {
 if(!msg.member.hasPermission('KICK_MEMBERS')) return msg.channel.send("You don't have permission to kick members.");
         let toKick = msg.mentions.members.first();
         let reason = args.slice(1).join(" ");
-        if(!args[0]) return msg.channel.send(`<a:errado:630163775122833419> | <@${message.author.id}>, Por favor, especifique um usuário\n\> **ex**: d-kick @user divulgação`);
-        if(!toKick) return msg.channel.send(`${args[0]} is not a member.`);
-        if(!reason) return msg.channel.send('Specify a reason.');
+        if(!args[0]) return msg.channel.send(`<a:errado:630163775122833419> | <@${message.author.id}>, Por favor, especifique um usuário\n\> **ex**: d-kick @user divulgação`).then(msg => msg.delete({timeout: 15000}))
+        if(!toKick) return msg.channel.send(`<a:errado:630163775122833419>| Isso **${args[0]}** não é um membro válido!`).then(msg => msg.delete({timeout: 15000}));
+        if(!reason) return msg.channel.send(`<a:errado:630163775122833419> | <@${message.author.id}> Neste comando em específico é necessário que você informe o motivo do kick!`).then(msg => msg.delete({timeout: 15000}));
  
         if(!toKick.kickable){
-            return msg.channel.send(':x: I cannot kick someone that is mod/admin. :x:');
+            return msg.channel.send(`<a:errado:630163775122833419> | Eu não posso kick em alguém que é moderador/administrador!`).then(msg => msg.delete({timeout: 15000}));
         }
  
         if(toKick.kickable){
