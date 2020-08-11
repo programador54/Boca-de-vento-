ebea089e2d3dbdd2b0669b8381a6020ad1aa1df5
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-11 12:27:54 -0300

    Create kick.js

---
+++ commands/kick.js
@@ -0,0 +1,24 @@
+if(!msg.member.hasPermission('KICK_MEMBERS')) return msg.channel.send("You don't have permission to kick members.");
+        let toKick = msg.mentions.members.first();
+        let reason = args.slice(1).join(" ");
+        if(!args[0]) return msg.channel.send('Please mention someone to kick');
+        if(!toKick) return msg.channel.send(`${args[0]} is not a member.`);
+        if(!reason) return msg.channel.send('Specify a reason.');
+ 
+        if(!toKick.kickable){
+            return msg.channel.send(':x: I cannot kick someone that is mod/admin. :x:');
+        }
+ 
+        if(toKick.kickable){
+            let x = new Discord.MessageEmbed()
+            .setTitle('Kick')
+            .addField('Member Kicked', toKick)
+            .addField('Kicked by', msg.author)
+            .addField('Reason', reason)
+            .addField('Date', msg.createdAt)
+            .setColor(r);
+ 
+            msg.channel.send(x);
+            toKick.kick();
+        }
+    }
