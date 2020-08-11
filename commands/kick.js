7badde389e3b3c14eb1ccbdf00b08fdefef2631a
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-11 12:56:52 -0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -6,12 +6,12 @@ module.exports = {
 
     async run (client, msg, args) {
 
-if(!msg.member.hasPermission('KICK_MEMBERS')) return msg.channel.send(`<a:alertA:727101012174962838> | <@${message.author.id}>, Você precisa ter a permissão de **EXPULSAR_MEMBROS** para poder utilizar este comando.`).then(msg => msg.delete({timeout: 15000}));
+if(!msg.member.hasPermission('KICK_MEMBERS')) return msg.channel.send(`<a:alertA:727101012174962838> | <@${msg.author.id}>, Você precisa ter a permissão de **EXPULSAR_MEMBROS** para poder utilizar este comando.`).then(msg => msg.delete({timeout: 15000}));
         let toKick = msg.mentions.members.first();
         let reason = args.slice(1).join(" ");
-        if(!args[0]) return msg.channel.send(`<a:errado:630163775122833419> | <@${message.author.id}>, Por favor, especifique um usuário\n\> **ex**: d-kick @user divulgação`).then(msg => msg.delete({timeout: 15000}))
+        if(!args[0]) return msg.channel.send(`<a:errado:630163775122833419> | <@${msg.author.id}>, Por favor, especifique um usuário\n\> **ex**: d-kick @user divulgação`).then(msg => msg.delete({timeout: 15000}))
         if(!toKick) return msg.channel.send(`<a:errado:630163775122833419>| Isso **${args[0]}** não é um membro válido!`).then(msg => msg.delete({timeout: 15000}));
-        if(!reason) return msg.channel.send(`<a:errado:630163775122833419> | <@${message.author.id}> Neste comando em específico é necessário que você informe o motivo do kick!`).then(msg => msg.delete({timeout: 15000}));
+        if(!reason) return msg.channel.send(`<a:errado:630163775122833419> | <@${msg.author.id}> Neste comando em específico é necessário que você informe o motivo do kick!`).then(msg => msg.delete({timeout: 15000}));
  
         if(!toKick.kickable){
             return msg.channel.send(`<a:errado:630163775122833419> | Eu não posso kick em alguém que é moderador/administrador!`).then(msg => msg.delete({timeout: 15000}));
