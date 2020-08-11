35bc22dfcaeef25a5237e9bcc55e4c06873ed9af
programador54 <52678460+programador54@users.noreply.github.com>
2020-08-11 12:40:30 -0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -9,7 +9,7 @@ module.exports = {
 if(!msg.member.hasPermission('KICK_MEMBERS')) return msg.channel.send("You don't have permission to kick members.");
         let toKick = msg.mentions.members.first();
         let reason = args.slice(1).join(" ");
-        if(!args[0]) return msg.channel.send('Please mention someone to kick');
+        if(!args[0]) return msg.channel.send(`<a:errado:630163775122833419> | <@${message.author.id}>, Por favor, especifique um usuário\n\> **ex**: d-kick @user divulgação`);
         if(!toKick) return msg.channel.send(`${args[0]} is not a member.`);
         if(!reason) return msg.channel.send('Specify a reason.');
  
